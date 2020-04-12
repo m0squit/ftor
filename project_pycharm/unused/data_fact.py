b9756f134d28c30f1ebe6767b5633658376cf7fd
m0squit <platyna@bk.ru>
2020-04-12 16:05:28 +0300

    chore delete unused files

--- project_pycharm/unused/data_fact.py
+++
@@ -1,9 +0,0 @@
-class DataFact(object):
-
-    def __init__(self,
-                 data,
-                 recovery_factor_start):
-        self._data = data
-        self._recovery_factor_start = recovery_factor_start
-
-
