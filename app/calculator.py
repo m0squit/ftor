0c8215dc64a45b22c2b3be612247aed51585f90a
m0squit <platyna@bk.ru>
2020-06-27 00:26:15 +0500

    refactor

--- app/calculator.py
+++ app/calculator.py
@@ -5,34 +5,58 @@ from domain.aggregates.project import Project
 class Calculator(object):
     """Domain objects calculation manager."""
 
+    _project: Project
+
     @classmethod
     def run(cls, project: Project) -> Project:
-        project = cls._calc_wells(project)
-        project.calc_metric()
-        return project
+        cls._project = project
+        cls._calc_wells()
+        cls._project.calc_metric()
+        return cls._project
 
-    @staticmethod
-    def _calc_wells(project: Project) -> Project:
-        data = pd.read_excel(io=project.settings.path / 'Отдельное.xlsx',
-                             sheet_name='Q_liq - модель',
-                             header=0,
-                             index_col=0,
-                             usecols='A:CM',
-                             nrows=22,
-                             na_values=0)
+    @classmethod
+    def _calc_wells(cls):
+        cls._add_prod_oil_ksg_to_df_test()
+        data = cls._read_model_liq_rates()
+        for well in cls._project.wells:
+            print(well.name)
+            rates_liq_model = data[well.name].to_list()
+            well.calc(rates_liq_model)
 
-        data.dropna(axis='index', how='all', inplace=True)
-        data = data.transpose()
-
-        for well in project.wells:
+    @classmethod
+    def _add_prod_oil_ksg_to_df_test(cls):
+        # Add new df_test column for prod_oil_ksg in each well
+        for well in cls._project.wells:
             well.report.df_test['prod_oil_ksg'] = None
 
+        dates = cls._project.wells[0].report.df_test.index
+
+        # Define prod_oil ratios in prod_oil_total for each well by first forecast day
+        ratios = {}
+        first_forecast_date = dates[0]
+        prod_oil_total = 0
+        for well in cls._project.wells:
+            prod_oil_total += well.report.df_test['prod_oil'].loc[first_forecast_date]
+        for well in cls._project.wells:
+            prod_oil_fact = well.report.df_test['prod_oil'].loc[first_forecast_date]
+            ratio = prod_oil_fact / prod_oil_total
+            ratios[well.name] = ratio
+
+        prods_oil_ksg = cls._read_ksg_oil_prods()
+        for i in range(len(prods_oil_ksg)):
+            prod_oil_ksg = prods_oil_ksg[i]
+            date = dates[i]
+            for well in cls._project.wells:
+                well.report.df_test['prod_oil_ksg'].loc[date] = prod_oil_ksg * ratios[well.name]
+
+    @classmethod
+    def _read_ksg_oil_prods(cls):
         read_params = {'02': {'col': 'K', 'skiprows': 10, 'nrows': 86},
                        '03': {'col': 'E', 'skiprows': 10, 'nrows': 92},
                        '04': {'col': 'F', 'skiprows': 10, 'nrows': 89}}
         prods_oil_ksg = []
         for sheet in read_params.keys():
-            s = pd.read_excel(io=project.settings.path / 'КСГ.xlsx',
+            s = pd.read_excel(io=cls._project.settings.path / 'ksg_oil_prods.xlsx',
                               sheet_name=sheet,
                               header=None,
                               usecols=read_params[sheet]['col'],
@@ -43,28 +67,17 @@ class Calculator(object):
             s = s.apply(lambda x: x / 0.849)
             s = s.to_list()
             prods_oil_ksg.extend(s)
+        return prods_oil_ksg
 
-        dates = project.wells[0].report.df_test.index
-
-        ratios = {}
-        date = dates[0]
-        prod_oil = 0
-        for well in project.wells:
-            prod_oil += well.report.df_test['prod_oil'].loc[date]
-        for well in project.wells:
-            prod_oil_fact = well.report.df_test['prod_oil'].loc[date]
-            ratio = prod_oil_fact / prod_oil
-            ratios[well.name] = ratio
-
-        for i in range(len(prods_oil_ksg)):
-            prod_oil_ksg = prods_oil_ksg[i]
-            date = dates[i]
-            for well in project.wells:
-                well.report.df_test['prod_oil_ksg'].loc[date] = prod_oil_ksg * ratios[well.name]
-
-        for well in project.wells:
-            print(well.name)
-            rates_liq_model = data[well.name].to_list()
-            well.calc(rates_liq_model)
-
-        return project
+    @classmethod
+    def _read_model_liq_rates(cls):
+        data = pd.read_excel(io=cls._project.settings.path / 'model_liq_rates.xlsx',
+                             sheet_name='Q_liq - модель',
+                             header=0,
+                             index_col=0,
+                             usecols='A:CM',
+                             nrows=22,
+                             na_values=0)
+        data.dropna(axis='index', how='all', inplace=True)
+        data = data.transpose()
+        return data
